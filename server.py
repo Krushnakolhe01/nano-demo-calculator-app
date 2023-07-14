897b9b263ea4027b482f67cf7c466667e8c874b4
Krushna Kolhe <krushnakolhe01@gmail.com>
2023-07-14 22:19:25 +0530

    Required changes been made to server.py

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ def add():
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
     data = request.get_json()
-    return { 'result': data['first']+data['second'] }
+    return { 'result': data['first']-data['second'] }
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
